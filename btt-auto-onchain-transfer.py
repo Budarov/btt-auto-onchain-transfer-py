5770a3324058a15007ee6314872901668df27326
Budarov <45418632+Budarov@users.noreply.github.com>
2021-06-02 11:41:41 +0300

    Update btt-auto-onchain-transfer.py
    Добавил логирование в файл

--- btt-auto-onchain-transfer.py
+++ btt-auto-onchain-transfer.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from datetime import datetime
+import os.path, sys
 
 
 #Указываем ваш порт speed.btt.network
@@ -14,7 +15,20 @@ min_transfer_sum = 1000000000
 
 #Получаем текующие дату и время
 current_time = datetime.now()
-current_time = current_time.strftime("%d-%m-%Y %H:%M:%S")
+current_time = current_time.strftime("%d-%m-%Y, %H:%M:%S")
+
+def to_log(massage):
+    #Получаем текующие дату и время
+    massage = ' ' + massage
+    current_time = datetime.now()
+    current_time = current_time.strftime("%d-%m-%Y, %H:%M:%S")
+    if os.path.isfile(sys.path[0] + '\\btt-auto-transfer.log'):
+        log_file = open(sys.path[0] + '\\btt-auto-transfer.log', 'a')
+    else:
+        log_file = open(sys.path[0] + '\\btt-auto-transfer.log', 'w')
+    print(current_time + massage)
+    log_file.write(current_time + massage + '\n')
+    log_file.close()
 
 
 #Получаем токен BTT Speed
@@ -23,7 +37,7 @@ def get_token(port):
         token_res = requests.get('http://127.0.0.1:' + str(port) + '/api/token')
         token = token_res.text
     except requests.ConnectionError:
-        print(current_time + ' Не удалось получить токен BTT Speed по адресу: ' + 'http://127.0.0.1:' + str(port) + '/api/token' + ' Указан неверный порт или не запущен BTT Speed.')
+        to_log('Не удалось получить токен BTT Speed по адресу: ' + 'http://127.0.0.1:' + str(port) + '/api/token' + ' Указан неверный порт или не запущен BTT Speed.')
         return ""
     return token
 
@@ -40,7 +54,7 @@ def get_tronscan_balance():
         balance = json.loads(balance_res.text)
         sa = list(filter(lambda tokenBalances: tokenBalances['tokenId'] == '1002000', balance["tokenBalances"]))
     except requests.ConnectionError:
-        print(current_time + ' Не удалось узнать балан шлюза по адресу https://apiasia.tronscan.io:5566/api/account?address=TA1EHWb1PymZ1qpBNfNj9uTaxd18ubrC7a сайт недоступен.')
+        to_log('Не удалось узнать балан шлюза по адресу https://apiasia.tronscan.io:5566/api/account?address=TA1EHWb1PymZ1qpBNfNj9uTaxd18ubrC7a сайт недоступен.')
         return 0
     return int(sa[0]['balance'])
     
@@ -50,17 +64,18 @@ def tranfer(port, token, transfer_sum):
     transfer_post = requests.post('http://127.0.0.1:' + str(port) + '/api/exchange/withdrawal?t=' + token + '&amount=' + str(transfer_sum))
     return transfer_post.text
 
+
 token = get_token(speed_btt_port)
 balance = get_balance(speed_btt_port, token)
 tronscan_balance = get_tronscan_balance()
 
 if (token != "") and (tronscan_balance > 0):
     if (tronscan_balance >= min_tronscan_balance) and (balance >= min_transfer_sum):
-        print(current_time + ' Баланс шлюза: ' + str(tronscan_balance / 1000000) + ' Баланс In App: ' + str(balance / 1000000) + ' Выполняется перевод.')
-        tr = tranfer(speed_btt_port, token, min_transfer_sum)
-        print('id транзакции: ' + tr)
+        to_log(current_time + ' Баланс шлюза: ' + str(tronscan_balance / 1000000) + ' Баланс In App: ' + str(balance / 1000000) + ' Выполняется перевод.')
+        #tr = tranfer(speed_btt_port, token, min_transfer_sum)
+        #to_log('id транзакции: ' + tr)
     else:
-        print(current_time + ' Баланс шлюза: ' + str(tronscan_balance / 1000000) + ' Баланс In App: ' + str(balance / 1000000) + ' Недостаточно средств In App или на шлюзе.')
+        to_log(current_time + ' Баланс шлюза: ' + str(tronscan_balance / 1000000) + ' Btt. Баланс In App: ' + str(balance / 1000000) + ' Btt. Недостаточно средств In App или на шлюзе.')
 else:
-    print(current_time + ' Не все необходимые данные удалось получить.')
+    to_log(current_time + ' Не все необходимые данные удалось получить.')
     
