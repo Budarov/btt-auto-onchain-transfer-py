530af1cd116b24f9f776b826fb57ac76daa104fc
Budarov <45418632+Budarov@users.noreply.github.com>
2021-06-06 18:38:59 +0300

    Update START.bat and some fixes

--- START.bat
+++ START.bat
@@ -1,4 +1,21 @@
-@echo off
-
-if exist C:\Python39\Lib\site-packages\requests  (python %~dp0\btt-auto-onchain-transfer.py) ^
-else (pip install requests && python btt-auto-onchain-transfer.py)
+@ECHO off
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`py -m pip show requests`) DO (
+  SET var!count!=%%F
+  SET /a count=!count!+1
+)
+set b=Name: requests
+if !var1! == !b! (ECHO requests module is already installed) ^
+else (py -m pip install requests)
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`py -m pip show pyTelegramBotAPI`) DO (
+  SET var!count!=%%F
+  SET /a count=!count!+1
+)
+set b=Name: pyTelegramBotAPI
+if !var1! == !b! (ECHO pyTelegramBotAPI module is already installed) ^
+else (py -m pip install pyTelegramBotAPI)
+if exist %~dp0\btt-auto-onchain-transfer.py  (py %~dp0\btt-auto-onchain-transfer.py) ^
+else (ECHO Script file not fount)
+ENDLOCAL
